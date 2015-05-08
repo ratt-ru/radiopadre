515fb44782ebb032ddc0351d57a9a54ee465abc0
Oleg Smirnov <osmirnov@gmail.com>
2015-05-08 21:30:27 +0200

    fixing notebooks

--- run-padre.sh
+++ run-padre.sh
@@ -1,5 +1,15 @@
-#!/bin/bash -ve
+#!/bin/bash 
 
 export PYTHONPATH=`pwd`
 
-ipython notebook --notebook-dir=.
+if [ "$SSH_CLIENT" != "" ]; then
+  opts="--no-browser"
+  echo "You're logged in via ssh, so I'm not opening a web browser for you. Please"
+  echo "manually browse to the indicated URL. You will probably want to employ ssh"
+  echo "port forwarding if you want to browse a remote notebook from your own machine."
+  
+else
+  opts = ""
+fi
+
+ipython notebook --notebook-dir=. $opts
