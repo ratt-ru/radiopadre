8d93c14b904d85f48d2e3899c3d9b186b25864f2
Gijs Molenaar <gijs@pythonic.nl>
2016-03-14 15:46:47 +0200

    add some tests that only do an import (better than nothing)

---
+++ tests/test_fitsfile.py
@@ -0,0 +1,7 @@
+import unittest
+import radiopadre.fitsfile
+
+
+class TestFitsFile(unittest.TestCase):
+    def test_fits_file(self):
+        pass
---
+++ tests/test_radiopadre.py
@@ -0,0 +1,7 @@
+import unittest
+import radiopadre
+
+
+class TestRadioPadre(unittest.TestCase):
+    def test_radiopadre(self):
+        pass
