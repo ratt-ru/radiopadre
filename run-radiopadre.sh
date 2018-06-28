c5fa1ca087c3d6c19bcaccc42ecee44bd593ca19
Oleg Smirnov <osmirnov@gmail.com>
2018-06-28 17:52:42 +0200

    Improved bootstrapping on server

--- run-radiopadre.sh
+++ run-radiopadre.sh
@@ -1,28 +1,42 @@
 #!/bin/bash 
 
-yn_prompt()
-{
-	echo -e "$1 (y/n)"
-	read
-	if [ $REPLY = 'y' ]; then
-	  return 0
-	fi
-	return 1
-}
 
-# Check for virtualenv first
-command -v virtualenv > /dev/null
+# get current directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ $? -eq 1 ]; then
-  if ! yn_prompt "virtualenv not found. Install with apt-get?"; then
-    echo -e "Radiopadre requires a working virtualenv, sorry."
-    exit 1 
+port=${RADIOPADRE_PORT:-$[$UID+9000]}
+opts="--notebook-dir=. --port=$[$UID+9000]"
+
+force_browser=""
+bootstrap=""
+
+while [ "$1" != "" ]; do
+  if [ "$1" == "-h" -o "$1" == "--help" ]; then
+    echo "Usage: $0 [-nb|--no-browser] [-b|--browser] [--bootstrap] [jupyter options] [notebook name]"
+    exit 0
+  elif [ "$1" == "-nb" -o "$1" == "--no-browser" ]; then
+    opts="$opts --no-browser"
+  elif [ "$1" == "-b" -o "$1" == "--browser" ]; then
+    force_browser=1
+  elif [ "$1" == "--bootstrap" ]; then
+    bootstrap=1
+  else
+    opts="$opts $1"
   fi
-  echo "Installing virtualenv"
-  if ! sudo apt-get install virtualenv; then
-    echo "virtualenv installation failed, see log above"
-    exit 1
-  fi
+  shift 1
+done
+
+if [ "$1" == "--bootstrap" ]; then
+  bootstrap="yes"
+else
+  bootstrap="no"
+fi
+
+if which virtualenv >/dev/null && which pip >/dev/null; then
+  echo "virtualenv and pip detected"
+else
+  echo "Radiopadre requires a working virtualenv and pip, sorry. Try apt install pip virtualenv?"
+  exit 1
 fi
 
 radpadre_path="${0%/*}"
@@ -40,47 +54,28 @@ if [ -f $radpadre_venv/bin/activate ]; then
   source $radpadre_venv/bin/activate
   echo "---"
 else
-  echo "Creating radiopadre virtual environment in $radpadre_venv and installing dependencies"
-  if ! yn_prompt "This is a one-off operation, and may take a few minutes. Proceed?"; then
+  if [ "$bootstrap" != "" ]; then
+    echo "The radiopadre virtual environment $radpadre_venv does not exist, creating it."
+    virtualenv $radpadre_venv
+
+    #launch virtual environment
+    source $radpadre_venv/bin/activate
+
+    #install the dependencies required for radiopadre
+    if ! pip install -r $radpadre_path/requirements.txt; then
+      echo "Some dependencies failed to install, see log above"
+      exit 1
+    fi
+    echo "All dependencies installed in virtual environment"
+    echo "---"
+  else
+    echo "The radiopadre virtual environment $radpadre_venv does not exist. If you would like to create it,"
+    echo "please re-run '$0 --bootstrap'. This may take a few minutes."  
     exit 1
   fi
-  echo "OK, proceeding..."
-  virtualenv $radpadre_venv
-
-  #launch virtual environment
-  source $radpadre_venv/bin/activate
-
-  #install the dependencies required for radiopadre
-  if ! pip install -r $radpadre_path/requirements.txt; then
-    echo "Some dependencies failed to install, see log above"
-    exit 1
-  fi
-  echo "All dependencies installed in virtual environment"
-  echo "---"
 fi
 
 
-# get current directory
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-port=${RADIOPADRE_PORT:-$[$UID+9000]}
-opts="--notebook-dir=. --port=$[$UID+9000]"
-
-force_browser=""
-
-while [ "$1" != "" ]; do
-  if [ "$1" == "-h" -o "$1" == "--help" ]; then
-    echo "Usage: $0 [-nb|--no-browser] [<notebook name>]"
-    exit 0
-  elif [ "$1" == "-nb" -o "$1" == "--no-browser" ]; then
-    opts="$opts --no-browser"
-  elif [ "$1" == "-b" -o "$1" == "--browser" ]; then
-    force_browser=1
-  else
-    opts="$opts $1"
-  fi
-  shift 1
-done
 
 # add the directory where run-radiopadre.sh resides to PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$DIR
@@ -110,7 +105,8 @@ echo Available notebooks: `find . -maxdepth 1 -name "*.ipynb"`
 echo
 opts="$opts --ContentsManager.pre_save_hook=radiopadre.notebook_utils._notebook_save_hook"
 
-echo "Running jupyter notebook $opts"
+echo "Command is: jupyter notebook $opts"
+echo "Please wait a moment for jupyter to start up..."
 jupyter notebook $opts &
 pid=$!
 
