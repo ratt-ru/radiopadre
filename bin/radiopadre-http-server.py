40e9ca220848d11a2f8d97e26bc79c8a1a0c56a1
Oleg Smirnov <osmirnov@gmail.com>
2021-12-01 15:05:42 +0200

    trying to debug failing http server under docker

--- bin/radiopadre-http-server.py
+++ bin/radiopadre-http-server.py
@@ -13,14 +13,14 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
 
     def translate_path(self, path):
         path = SimpleHTTPRequestHandler.translate_path(self, path)
-        print("HTTPServer: requested {}".format(path))
+        print("HTTPServer: requested {}".format(path), file=sys.stderr)
         if not path.startswith(path_id):
-            print("HTTPServer: ignoring request for {}".format(path))
+            print("HTTPServer: ignoring request for {}".format(path), file=sys.stderr)
             return "/dev/null"
         for src, dest in path_rewrites:
             if path.startswith(src):
                 newpath = dest + path[len(src):]
-                print("HTTPServer: rewriting {}->{}".format(path, newpath))
+                print("HTTPServer: rewriting {}->{}".format(path, newpath), file=sys.stderr)
                 return newpath
         return path
 
@@ -31,15 +31,15 @@ if __name__ == '__main__':
     for arg in sys.argv[1:]:
         if re.match("\d+$", arg):
             port = int(arg)
-            print(f"HTTPServer: using port {port}")
+            print(f"HTTPServer: using port {port}", file=sys.stderr)
         elif arg.endswith(".pem"):
             ssl_cert = arg
-            print(f"HTTPServer: using SSL certificate {ssl_cert}")
+            print(f"HTTPServer: using SSL certificate {ssl_cert}", file=sys.stderr)
         elif "=" in arg:
             src, dest = arg.split("=", 1)
             src = path_id + src
             path_rewrites.insert(0, (src, dest))
-            print("HTTPServer: will rewrite {}->{}".format(src, dest))
+            print("HTTPServer: will rewrite {}->{}".format(src, dest), file=sys.stderr)
     
 #    server_address = ("0.0.0.0", port)
 #    server_address = ('localhost', port)
